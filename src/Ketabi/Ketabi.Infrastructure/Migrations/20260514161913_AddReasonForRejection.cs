126e9998481dd53b78a0c38974455d5c2a76588e
Mustafa <mustafawork911@gmail.com>
2026-05-14 19:20:05 +0300

    add ReasonForRejection migration

---
+++ src/Ketabi/Ketabi.Infrastructure/Migrations/20260514161913_AddReasonForRejection.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ketabi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReasonForRejection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ReasonForRejection",
+                table: "BookListing",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReasonForRejection",
+                table: "BookListing");
+        }
+    }
+}
